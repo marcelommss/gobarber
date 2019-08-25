b8df9ddde816604bbe2a67da8199cc736257bfee
Marcelo Moraes Motta de Mattos <marcelomattos@live.com>
2019-08-24 21:25:56 -0300

    fix: not calling init on database/index.js ctor.

--- src/database/index.js
+++ src/database/index.js
@@ -7,13 +7,12 @@ import databaseConfig from '../config/database';
 const models = [User];
 
 class Database {
-  contructors() {
+  contructor() {
     this.init();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-
     models.map(model => model.init(this.connection));
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,26 @@
 import { Router } from 'express';
 import User from './app/models/User';
+import database from './database';
 
 const routes = new Router();
 
 routes.get('/', async (req, res) => {
-  const user = await User.create({
+  // solução temp
+  // Por que não está chamando o init() do database/index.js
+  // no arquivo app.js >> import './database'; ??
+  database.init();
+
+  await User.create({
     name: 'Marcelo Mattos',
     email: 'marcelo@inblu.com.br',
     password_hash: '1324314167',
-  });
-  // .catch(err => {
-  //   return res.json(err);
-  // });
-  return res.json(user);
+  })
+    .then(data => {
+      return res.json(data);
+    })
+    .catch(err => {
+      return res.json(err);
+    });
 });
 
 export default routes;
