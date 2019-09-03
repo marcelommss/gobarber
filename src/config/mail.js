cbb76a07ffcba183fe46fa35c3d72ee8861fecc3
Marcelo Moraes Motta de Mattos <marcelomattos@live.com>
2019-09-03 00:03:30 -0300

    chore: env variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '4b7355df5312e8',
-    pass: 'c2267b8a09c9fb',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
